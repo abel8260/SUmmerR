74a3193d501ff96065be742301aa5a241d1fc6ac
Abel Barbosa <78666074+abel8260@users.noreply.github.com>
2022-01-28 16:51:06 -0300

    Create loglog.pas

---
+++ loglog.pas
@@ -0,0 +1,67 @@
+unit loglog;
+interface 
+
+procedure Menuloglogg();
+
+
+
+implementation
+uses crt, SysUtils;
+
+
+procedure Menuloglogg();
+
+
+
+
+var 
+     
+  name_idt : string;
+  name_idtt : string; 
+  MyFilee: text;
+  
+     
+    
+    
+
+
+BEGIN
+     
+  
+	
+	writeln('|folder:processing.');
+	
+	writeln('|making the subs for you: ');
+	
+	
+	
+
+      
+	//cripto_process
+	writeln('| name of the folder:');
+	readln(name_idt);
+	writeln('| log to add of the folder:');
+	readln(name_idtt);
+	name_idt :=  'folder-'+name_idt+'-log-'+name_idtt+'.txt';
+	assign(MyFilee,name_idt);
+	
+
+
+    rewrite(MyFilee); 
+    reset(MyFilee);    //Reopen the file for reading
+    //readln(MyFilee, name_idt);
+    writeln('Text read from file: ', name_idt);
+   
+ 
+    append(MyFilee);
+    writeln(MyFilee, name_idt); 
+  
+
+  
+    close(MyFilee);
+	
+	
+END;
+
+
+end.
