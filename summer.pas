bad22365848ec0314137e29b992e7e92d4104d74
Abel Barbosa <78666074+abel8260@users.noreply.github.com>
2022-01-28 17:02:42 -0300

    Create summer.pas

---
+++ summer.pas
@@ -0,0 +1,41 @@
+program summer;
+
+uses crt, menubcrise, menuhitech, registros, dasummer, 
+menuiochapram, menuregpt, menupppd, estagiarium, estagiario,  
+menupluspram;
+
+
+
+
+var esc: string; 
+    
+    
+
+
+BEGIN
+    
+    
+      
+     
+     writeln('############');
+     writeln('Menu da summer:');
+     writeln('------------');   
+     writeln('=> estagiario -> estagiario; ');   
+     writeln('=> r -> ia; '); 
+     writeln('------------'); 
+     write('R.: ');     
+     readln(esc); 
+    
+    
+    if(esc = 'estagiario') then 
+      Menudoestagiarium()
+       
+      else if(esc = 'r') then 
+        Menudasummer()
+     
+      else
+      writeln('o usuario n digitou a opçao registrada');    
+      
+   
+      
+END.
